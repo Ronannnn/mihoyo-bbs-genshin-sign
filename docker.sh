79a7529d2bc0026ee4a9eb6619f1b7e12f602d40
Ronan <crnoogle@gmail.com>
2022-08-21 12:28:28 +0800

    feat(build): Optimize docker build script

--- docker.sh
+++ docker.sh
@@ -1,9 +1,15 @@
-#docker build --platform linux/arm64/v8 -t ronannnn/mihoyo-bbs-genshin-sign:0.1.0 -f Dockerfile .
-docker build -t ronannnn/mihoyo-bbs-genshin-sign:0.1.0 -f Dockerfile .
-docker push ronannnn/mihoyo-bbs-genshin-sign:0.1.0
-# docker save -o image-go.tar ronannnn/jrs-go:1.0
-# scp image-go.tar liu_c5@10.70.9.111:~/image-tar/
-# scp image-go.tar liu_c5@10.70.9.112:~/image-tar/
+#!/bin/zsh
 
-# mysql -h 127.0.0.1 -P 3306 -u root -p****** --default-character-set=utf8 jrs < /Users/ronan/Downloads/backup_2022-04-11T20:00:21Z.sql
-# mysqldump -h 127.0.0.1 -P 3306 -u root -proot --ssl-mode=DISABLED --skip-add-locks --column-statistics=0 jrs >/Users/ronan/Desktop/jrs_2022-04-12_092007.sql
+image_tag=$1
+if [ "$image_tag" = "" ]; then
+  echo "Usage: $0 <image_tag>"
+  exit 0
+fi
+
+image_name_prefix="ronannnn/mihoyo-bbs-genshin-sign"
+docker build -t "${image_name_prefix}:${image_tag}" -f Dockerfile .
+docker push "${image_name_prefix}:${image_tag}"
+
+# tag as latest and push
+docker tag "${image_name_prefix}:${image_tag}" "${image_name_prefix}:latest"
+docker push "${image_name_prefix}:latest"
